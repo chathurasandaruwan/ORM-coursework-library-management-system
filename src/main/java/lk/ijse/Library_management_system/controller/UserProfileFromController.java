5c79894e5808f1e1426106f3b44199fb8d38ed2c
chathura <chathurasandaruwan122@gmail.com>
2024-03-05 19:55:46 +0530

    create-user_books_profile-ui

--- src/main/java/lk/ijse/Library_management_system/controller/UserProfileFromController.java
+++ src/main/java/lk/ijse/Library_management_system/controller/UserProfileFromController.java
@@ -1,4 +1,47 @@
 package lk.ijse.Library_management_system.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class UserProfileFromController {
+
+    @FXML
+    private TextField textConfirmPassword;
+
+    @FXML
+    private TextField textNewPassword;
+
+    @FXML
+    private TextField textEmail;
+
+    @FXML
+    private TextField textUserName;
+
+    @FXML
+    private Label lblReturnBookCount;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    void btnDeletOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
