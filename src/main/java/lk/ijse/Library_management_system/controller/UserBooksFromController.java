956c8838ccc1b7b5eaa25c53d673823af4b28f53
chathura <chathurasandaruwan122@gmail.com>
2024-03-05 16:52:25 +0530

    create-user_books_form

--- src/main/java/lk/ijse/Library_management_system/controller/UserBooksFromController.java
+++ src/main/java/lk/ijse/Library_management_system/controller/UserBooksFromController.java
@@ -1,4 +1,64 @@
 package lk.ijse.Library_management_system.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class UserBooksFromController {
+
+    @FXML
+    private TableView<?> tblBrrowed;
+
+    @FXML
+    private TableView<?> tblBookList;
+
+    @FXML
+    private TextField textSearch;
+
+    @FXML
+    private ComboBox<?> CombSearchValues;
+
+    @FXML
+    private Label lblBookTitle;
+
+    @FXML
+    private Label lblAthorName;
+
+    @FXML
+    private Label lblGen;
+
+    @FXML
+    private Label lblBookId;
+
+    @FXML
+    private Label lblStatus;
+
+    @FXML
+    private Button btnReturn;
+
+    @FXML
+    private Button btnBorrow;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    void btnBorrowOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
 }
