8baf3d2b173a1744e1cc893fe98e76f585754547
chathura <chathurasandaruwan122@gmail.com>
2024-03-05 19:16:45 +0530

    create-user_books_history_form

--- src/main/java/lk/ijse/Library_management_system/controller/BooksHistoryFromController.java
+++ src/main/java/lk/ijse/Library_management_system/controller/BooksHistoryFromController.java
@@ -1,4 +1,15 @@
 package lk.ijse.Library_management_system.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
 public class BooksHistoryFromController {
+
+    @FXML
+    private TableView<?> tblView;
+
+    @FXML
+    private Label lblDate;
+
 }
