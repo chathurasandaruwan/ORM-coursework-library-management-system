9a1c3b78df8b6beb0c30ee96aeeb84873987da2a
chathura <chathurasandaruwan122@gmail.com>
2024-03-05 15:41:45 +0530

    edit-admin_books_form

--- src/main/java/lk/ijse/Library_management_system/controller/AdminBranchFromController.java
+++ src/main/java/lk/ijse/Library_management_system/controller/AdminBranchFromController.java
@@ -1,4 +1,40 @@
 package lk.ijse.Library_management_system.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class AdminBranchFromController {
+
+    @FXML
+    private TextField textId;
+
+    @FXML
+    private TextField textAddress;
+
+    @FXML
+    private DatePicker OpenedDayPiker;
+
+    @FXML
+    private TableView<?> tblView;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
 }
