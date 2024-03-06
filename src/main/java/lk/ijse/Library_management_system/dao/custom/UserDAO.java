927982f064977d8fa9955ddd4c40e5b3c2ae1ef5
chathura <chathurasandaruwan122@gmail.com>
2024-03-06 11:01:32 +0530

    create-DAO-layers

---
+++ src/main/java/lk/ijse/Library_management_system/dao/custom/UserDAO.java
@@ -0,0 +1,4 @@
+package lk.ijse.Library_management_system.dao.custom;
+
+public interface UserDAO {
+}
---
+++ src/main/java/lk/ijse/Library_management_system/dao/custom/impl/UserDAOImpl.java
@@ -0,0 +1,4 @@
+package lk.ijse.Library_management_system.dao.custom.impl;
+
+public class UserDAOImpl {
+}
