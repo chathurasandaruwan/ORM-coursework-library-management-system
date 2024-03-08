93f859527a3c87ff04686d63451c959902d2b63e
chathura <chathurasandaruwan122@gmail.com>
2024-03-08 12:13:20 +0530

    add-getter-setter-to-Book-entity

--- src/main/java/lk/ijse/Library_management_system/entity/Book.java
+++ src/main/java/lk/ijse/Library_management_system/entity/Book.java
@@ -15,6 +15,63 @@ public class Book {
     @JoinColumn(name = "branch_id")
     private Branch branch;
 
+    public Book() {
+    }
 
+    public Book(int id, String title, String author, int availabilityStatus, String generation, Branch branch) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.availabilityStatus = availabilityStatus;
+        this.generation = generation;
+        this.branch = branch;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getAvailabilityStatus() {
+        return availabilityStatus;
+    }
+
+    public void setAvailabilityStatus(int availabilityStatus) {
+        this.availabilityStatus = availabilityStatus;
+    }
+
+    public String getGeneration() {
+        return generation;
+    }
+
+    public void setGeneration(String generation) {
+        this.generation = generation;
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
+
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
 }
