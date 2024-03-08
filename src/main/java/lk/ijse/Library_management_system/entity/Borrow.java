59b41551e64c4735035f56348827e59efac2d2d5
chathura <chathurasandaruwan122@gmail.com>
2024-03-08 12:12:31 +0530

    add-getter-setter-to-Borrow-entity

--- src/main/java/lk/ijse/Library_management_system/entity/Borrow.java
+++ src/main/java/lk/ijse/Library_management_system/entity/Borrow.java
@@ -18,5 +18,54 @@ public class Borrow {
     @JoinColumn(name = "book_id")
     private Book book;
 
+    public Borrow() {
+    }
 
+    public Borrow(int id, LocalDate borrowedDate, LocalDate returningDate, User user, Book book) {
+        this.id = id;
+        this.borrowedDate = borrowedDate;
+        this.returningDate = returningDate;
+        this.user = user;
+        this.book = book;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getBorrowedDate() {
+        return borrowedDate;
+    }
+
+    public void setBorrowedDate(LocalDate borrowedDate) {
+        this.borrowedDate = borrowedDate;
+    }
+
+    public LocalDate getReturningDate() {
+        return returningDate;
+    }
+
+    public void setReturningDate(LocalDate returningDate) {
+        this.returningDate = returningDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
 }
