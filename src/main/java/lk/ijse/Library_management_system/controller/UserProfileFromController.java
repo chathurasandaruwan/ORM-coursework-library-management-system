3d3de5589e1b71a9d697743758daa5e4fff1d2c3
chathura <chathurasandaruwan122@gmail.com>
2024-03-10 23:30:32 +0530

    Add-verification-before-user-update

--- src/main/java/lk/ijse/Library_management_system/controller/UserProfileFromController.java
+++ src/main/java/lk/ijse/Library_management_system/controller/UserProfileFromController.java
@@ -5,6 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import lk.ijse.Library_management_system.bo.custom.UserBO;
+import lk.ijse.Library_management_system.bo.custom.impl.UserBOImpl;
+import lk.ijse.Library_management_system.dto.UserDTO;
+
+import java.util.List;
 
 public class UserProfileFromController {
 
@@ -28,7 +33,33 @@ public class UserProfileFromController {
 
     @FXML
     private Button btnUpdate;
+    @FXML
+    private Label lblUserId;
+    UserBO userBO = new UserBOImpl();
+    public void initialize() {
+        textNewPassword.setDisable(true);
+        textConfirmPassword.setDisable(true);
+        btnUpdate.setDisable(true);
+    }
+    @FXML
+    void textEmailOnAction(ActionEvent event) {
+        String name = textUserName.getText();
+        String email = textEmail.getText();
+        List<UserDTO> users = userBO.getAllUser();
+        for (UserDTO user : users) {
+            if (name.equals(user.getName())){
+                if (email.equals(user.getEmail())){
+                    textNewPassword.setDisable(false);
+                    textConfirmPassword.setDisable(false);
+                    btnUpdate.setDisable(false);
+                    lblUserId.setText(String.valueOf(user.getId()));
+                    return;
+                }
+            }
+        }
+        System.out.println("Not Found");
 
+    }
     @FXML
     void btnDeletOnAction(ActionEvent event) {
 
