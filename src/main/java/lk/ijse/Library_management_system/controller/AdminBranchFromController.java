6626337f3da57518f36a78fa04af14fd067e2b55
chathura <chathurasandaruwan122@gmail.com>
2024-03-11 14:46:50 +0530

    create-branch-loadAll-to-table

--- src/main/java/lk/ijse/Library_management_system/controller/AdminBranchFromController.java
+++ src/main/java/lk/ijse/Library_management_system/controller/AdminBranchFromController.java
@@ -3,10 +3,12 @@ package lk.ijse.Library_management_system.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import lk.ijse.Library_management_system.bo.custom.BranchBO;
 import lk.ijse.Library_management_system.bo.custom.impl.BranchBOImpl;
 import lk.ijse.Library_management_system.dto.BranchDTO;
 import lk.ijse.Library_management_system.entity.Branch;
+import lk.ijse.Library_management_system.tdm.BranchTM;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -23,19 +25,36 @@ public class AdminBranchFromController {
     private DatePicker OpenedDayPiker;
 
     @FXML
-    private TableView<?> tblView;
+    private TableView<BranchTM> tblView;
 
     @FXML
     private Button btnDelete;
 
     @FXML
     private Button btnSave;
+
     @FXML
     private Button btnAddNew;
+
+    @FXML
+    private TableColumn<?, ?> columnId;
+
+    @FXML
+    private TableColumn<?, ?> columnAddress;
+
+    @FXML
+    private TableColumn<BranchTM, LocalDate> columnOpenDate;
     BranchBO branchBO = new BranchBOImpl();
     public void initialize() {
+
+        setvaluesFactory();
         loadAllBranch();
         resetAll();
+    }
+    private void setvaluesFactory() {
+        columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        columnAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        columnOpenDate.setCellValueFactory(new PropertyValueFactory<>("openedDate"));
     }
     public void resetAll(){
         textId.setOpacity(0);
@@ -66,12 +85,14 @@ public class AdminBranchFromController {
             new Alert(Alert.AlertType.INFORMATION,"SAVE SUCCESS !!!").show();
             textAddress.clear();
             OpenedDayPiker.setValue(null);
+            loadAllBranch();
         }
     }
     public void loadAllBranch(){
+        tblView.getItems().clear();
         List<BranchDTO> branches =branchBO.getAllBranch();
         for (BranchDTO branch : branches) {
-            System.out.println(branch.getAddress());
+            tblView.getItems().add(new BranchTM(branch.getId(),branch.getAddress(),branch.getOpenedDate()));
         }
     }
 
