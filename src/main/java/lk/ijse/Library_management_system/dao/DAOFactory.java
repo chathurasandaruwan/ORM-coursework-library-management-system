9cb256dc18b3f5b2f18555b47f042f76ef9bd6c7
chathura <chathurasandaruwan122@gmail.com>
2024-03-14 09:41:54 +0530

    create-DAOFactory

---
+++ src/main/java/lk/ijse/Library_management_system/dao/DAOFactory.java
@@ -0,0 +1,30 @@
+package lk.ijse.Library_management_system.dao;
+
+import lk.ijse.Library_management_system.dao.custom.impl.BookDAOImpl;
+import lk.ijse.Library_management_system.dao.custom.impl.BranchDAOImpl;
+import lk.ijse.Library_management_system.dao.custom.impl.UserDAOImpl;
+
+public class DAOFactory {
+    private static DAOFactory daoFactory;
+
+    private DAOFactory() {
+    }
+    public static DAOFactory getDaoFactory(){
+        return daoFactory == null ? daoFactory = new DAOFactory() : daoFactory;
+    }
+    public enum DAOType{
+        BOOK,BRANCH,USER
+    }
+    public SupperDAO getDAO(DAOType daoType){
+        switch (daoType){
+            case BOOK:
+                return new BookDAOImpl();
+            case USER:
+                return new UserDAOImpl();
+            case BRANCH:
+                return new BranchDAOImpl();
+            default:
+                return null;
+        }
+    }
+}
