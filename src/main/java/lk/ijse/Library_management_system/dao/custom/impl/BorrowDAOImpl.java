a56ab238029c3f99915c1085a209ffe43c901b5d
chathura <chathurasandaruwan122@gmail.com>
2024-03-14 19:51:05 +0530

    add-isSaved-to-borrowSave

--- src/main/java/lk/ijse/Library_management_system/dao/custom/impl/BorrowDAOImpl.java
+++ src/main/java/lk/ijse/Library_management_system/dao/custom/impl/BorrowDAOImpl.java
@@ -3,14 +3,19 @@ package lk.ijse.Library_management_system.dao.custom.impl;
 import lk.ijse.Library_management_system.config.FactoryConfiguration;
 import lk.ijse.Library_management_system.dao.custom.BorrowDAO;
 import lk.ijse.Library_management_system.entity.Borrow;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 public class BorrowDAOImpl implements BorrowDAO {
     @Override
     public boolean save(Borrow entity) {
-        Session session = FactoryConfiguration.getInstance().getSession();
-        session.save(entity);
-        session.close();
-        return true;
+        try {
+            Session session = FactoryConfiguration.getInstance().getSession();
+            session.save(entity);
+            session.close();
+            return true;
+        }catch (HibernateException e){
+            return false;
+        }
     }
 }
