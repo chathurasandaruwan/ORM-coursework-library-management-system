d1d2ac1d9ad646b3125e227286f12223e21393bb
chathura <chathurasandaruwan122@gmail.com>
2024-03-14 09:46:16 +0530

    APPLY-DAOFactory-to-UserBOImpl

--- src/main/java/lk/ijse/Library_management_system/bo/custom/impl/UserBOImpl.java
+++ src/main/java/lk/ijse/Library_management_system/bo/custom/impl/UserBOImpl.java
@@ -1,6 +1,7 @@
 package lk.ijse.Library_management_system.bo.custom.impl;
 
 import lk.ijse.Library_management_system.bo.custom.UserBO;
+import lk.ijse.Library_management_system.dao.DAOFactory;
 import lk.ijse.Library_management_system.dao.custom.UserDAO;
 import lk.ijse.Library_management_system.dao.custom.impl.UserDAOImpl;
 import lk.ijse.Library_management_system.dto.UserDTO;
@@ -10,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserBOImpl implements UserBO {
-    UserDAO userDAO = new UserDAOImpl();
+    UserDAO userDAO = (UserDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOType.USER);
     @Override
     public boolean saveUser(UserDTO dto){
         return userDAO.save(new User(dto.getName(), dto.getEmail(), dto.getPassword()));
