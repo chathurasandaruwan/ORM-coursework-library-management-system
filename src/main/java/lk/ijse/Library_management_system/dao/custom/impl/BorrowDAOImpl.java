a52bc30e0a398867dcbef4510658589565c36d5f
chathura <chathurasandaruwan122@gmail.com>
2024-03-14 10:42:16 +0530

    create-BorrowDAO-layer

---
+++ src/main/java/lk/ijse/Library_management_system/dao/custom/impl/BorrowDAOImpl.java
@@ -0,0 +1,7 @@
+package lk.ijse.Library_management_system.dao.custom.impl;
+
+import lk.ijse.Library_management_system.dao.custom.BorrowDAO;
+
+public class BorrowDAOImpl implements BorrowDAO {
+
+}
