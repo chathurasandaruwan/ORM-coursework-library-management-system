0ef3139e267f0ddae0668d97e8b2fa39442d428e
chathura <chathurasandaruwan122@gmail.com>
2024-03-15 18:27:27 +0530

    add-CrudDAO-to-userDAOImpl

--- src/main/java/lk/ijse/Library_management_system/dao/custom/UserDAO.java
+++ src/main/java/lk/ijse/Library_management_system/dao/custom/UserDAO.java
@@ -1,11 +1,12 @@
 package lk.ijse.Library_management_system.dao.custom;
 
+import lk.ijse.Library_management_system.dao.CrudDAO;
 import lk.ijse.Library_management_system.dao.SupperDAO;
 import lk.ijse.Library_management_system.entity.User;
 
 import java.util.List;
 
-public interface UserDAO extends SupperDAO {
+public interface UserDAO extends CrudDAO<User> {
     boolean save(User entity);
 
     List<User> getAll();
