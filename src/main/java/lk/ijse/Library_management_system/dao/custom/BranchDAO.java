3a7a6486b6056526019238d7a766a4f4ae57e3d3
chathura <chathurasandaruwan122@gmail.com>
2024-03-15 18:26:39 +0530

    add-CrudDAO-to-branchDAOImpl

--- src/main/java/lk/ijse/Library_management_system/dao/custom/BranchDAO.java
+++ src/main/java/lk/ijse/Library_management_system/dao/custom/BranchDAO.java
@@ -1,11 +1,12 @@
 package lk.ijse.Library_management_system.dao.custom;
 
+import lk.ijse.Library_management_system.dao.CrudDAO;
 import lk.ijse.Library_management_system.dao.SupperDAO;
 import lk.ijse.Library_management_system.entity.Branch;
 
 import java.util.List;
 
-public interface BranchDAO extends SupperDAO {
+public interface BranchDAO extends CrudDAO<Branch> {
     boolean save(Branch entity);
 
     List<Branch> getAll();
